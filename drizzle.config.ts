c7180efad1a231532c1882b400a58235b1ac6500
mathiasgugu <matgum51873@stud.noroff.no>
2024-07-18 21:59:59 +0200

    Fix linting and drizzle package

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import "dotenv/config"; // make sure to install dotenv package
-import { defineConfig } from "drizzle-kit";
+import 'dotenv/config'; // make sure to install dotenv package
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  dialect: "postgresql",
-  out: "./drizzle",
-  schema: "./drizzle/schema.ts",
+  dialect: 'postgresql',
+  out: './drizzle',
+  schema: './drizzle/schema.ts',
   dbCredentials: {
     url: process.env.DATABASE_PUBLIC_URL!,
   },
--- drizzle/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
-
-export const streak = pgTable("streak", {
-  id: serial("id").primaryKey().notNull(),
-});
